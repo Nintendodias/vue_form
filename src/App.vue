d58327de9b906ba5f75c30b1b7244d7a7b459b4d
borisovaas <borisovaas@oooinex.ru>
2021-05-27 01:37:23 +0300

    first page (form)
    Create form layout, add libraries, add routing

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,45 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">MAIN</router-link> |
-      <router-link to="/results">RESULTS</router-link>
-    </div>
     <transition name="fade">
       <router-view />
     </transition>
   </div>
 </template>
+<script>
+import Vue from "vue";
+
+//bootstrap
+import { BootstrapVue } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+
+export default {};
+</script>
 
 <style lang="less">
+@import 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css';
 @import "./scss/animation.less";
-
+@import "./scss/variables.less";
 
 #app {
   font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 12px;
+
+  .section {
+    max-width: @section-max-width;
+    width: 100%;
+  }
 }
 </style>
--- src/views/page_main.vue
+++ src/views/page_main.vue
@@ -1,40 +1,158 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <ValidationProvider rules="secret" v-slot="{ errors }">
-      <input v-model="email" type="text" />
-      <span>{{ errors[0] }}</span>
-    </ValidationProvider>
-  </div>
+  <section class="section section--form">
+    <b-form
+      class="form__request"
+      @submit.prevent="onSubmit"
+      @reset.prevent="onReset"
+    >
+      <b-form-group id="form-name" label="Ваше ФИО:" label-for="name">
+        <b-form-input
+          id="name"
+          v-model="form.name"
+          placeholder="Введите ФИО"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="form-age" label="Сколько вам полных лет:" label-for="age">
+        <b-form-input
+          id="age"
+          v-model="form.age"
+          placeholder="Введите возраст"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        id="form-date"
+        label="Выберите дату посещения"
+        label-for="date"
+      >
+        <b-form-datepicker
+          id="date"
+          v-model="form.calendar"
+          start-weekday="1"
+          class="form-control"
+          menu-class="w-100"
+          calendar-width="100%"
+          placeholder="Выберите дату"
+          :min="today"
+          :max="lastDate"
+        ></b-form-datepicker>
+      </b-form-group>
+
+      <b-form-group
+        id="form-time"
+        label="Выберите интервал приема"
+        label-for="time"
+      >
+        <b-form-select
+          id="time"
+          v-model="form.time"
+          :options="interval"
+          class="form-control"
+        ></b-form-select>
+      </b-form-group>
+
+      <div class="form__btns">
+        <b-button type="submit" variant="primary">Записаться</b-button>
+      </div>
+    </b-form>
+  </section>
 </template>
 
 <script>
-//bootstrap
 import Vue from "vue";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
 
 //VeeValidate
-import { ValidationProvider, extend } from "vee-validate";
+import VeeValidate from "vee-validate";
 
-import HelloWorld from "@/components/HelloWorld.vue";
-
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
-
-Vue.use(ValidationProvider);
-extend('secret', {
-  validate: value => value === 'example',
-  message: 'This is not the magic word'
+Vue.use(VeeValidate, {
+  classes: true,
 });
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-    ValidationProvider,
+  data() {
+    return {
+      form: {
+        name: "",
+        time: null,
+        calendar: "",
+        age: ""
+      },
+      interval: [
+        { text: "Выберите время:", value: null },
+        "9.00 - 10.00",
+        "10.00 - 11.00",
+        "11.00 - 12.00",
+        "12.00 - 13.00",
+        "13.00 - 14.00",
+        "14.00 - 15.00",
+        "15.00 - 16.00",
+        "16.00 - 17.00",
+        "17.00 - 18.00",
+        "18.00 - 19.00",
+      ],
+      // show: true,
+    };
+  },
+  computed: {
+    today() {
+      return new Date();
+    },
+    lastDate() {
+      let max = new Date();
+      max.setDate(max.getDate() + 14);
+      return max;
+    },
+  },
+  methods: {
+    onSubmit() {
+      //Если пройдет валидацию, то router push, router-link убрать
+      alert(JSON.stringify(this.form));
+    },
+    onReset() {
+      this.form.name = "";
+      this.form.time = null;
+      this.show = false;
+      this.data = "";
+      // this.$nextTick(() => {
+      //   this.show = true;
+      // });
+    },
   },
 };
 </script>
+
+<style lang="less">
+.section--form {
+  label {
+    font-size: 1.5em;
+    margin-bottom: 0.5em;
+  }
+
+  input,
+  select,
+  option {
+    font-size: 1.2em;
+    color: #212529 !important;
+  }
+
+  .form-group {
+    margin-bottom: 2em;
+
+    &#form-date {
+      .btn-outline-primary {
+        border: 2px solid #0d6efd !important;
+      }
+      .btn:not(.disabled) {
+        font-weight: bold;
+      }
+    }
+  }
+
+  .form__btns {
+    .btn {
+      width: 100%;
+    }
+  }
+}
+</style>
