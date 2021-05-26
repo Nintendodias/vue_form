b7982a850d075af19a318b4efa8249917bd5286e
borisovaas <borisovaas@oooinex.ru>
2021-05-26 21:55:41 +0300

    add libraries

--- src/views/page_main.vue
+++ src/views/page_main.vue
@@ -2,27 +2,39 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ValidationProvider rules="secret" v-slot="{ errors }">
+      <input v-model="email" type="text" />
+      <span>{{ errors[0] }}</span>
+    </ValidationProvider>
   </div>
 </template>
 
 <script>
 //bootstrap
-import Vue from 'vue';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 //VeeValidate
+import { ValidationProvider, extend } from "vee-validate";
 
 import HelloWorld from "@/components/HelloWorld.vue";
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
+Vue.use(ValidationProvider);
+extend('secret', {
+  validate: value => value === 'example',
+  message: 'This is not the magic word'
+});
+
 export default {
   name: "Home",
   components: {
-    HelloWorld, 
+    HelloWorld,
+    ValidationProvider,
   },
 };
 </script>
